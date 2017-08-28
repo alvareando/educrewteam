2bd87865dba9ea3a936d602bfbcd4c91ee3feea8
laury meredith <meredith.laury@gmail.com>
2017-08-28 18:05:58 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :sessions, only: [:index, :show, :new, :create] do
+    resources :session_participation, only: [:create]
+    resources :chatrooms, only: [:create]
+  end
+
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: [:index, :create, :destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
