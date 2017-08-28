0291a3ca8ca88bb628c79be0440f2cf7c2e2d397
Alvaro Lobato <alobato678@gmail.com>
2017-08-28 18:04:14 +0100

    did controllers

--- db/migrate/20170828154159_create_session_participations.rb
+++ db/migrate/20170828154159_create_session_participations.rb
@@ -1,7 +1,7 @@
 class CreateSessionParticipations < ActiveRecord::Migration[5.0]
   def change
     create_table :session_participations do |t|
-      t.references :student, foreign_key: { to_table: :users }
+      t.references :students, foreign_key: { to_table: :users }
       t.references :sessions, foreign_key: true
 
       t.timestamps
