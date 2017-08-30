b013b112f9c7c4a3561f35853fa77a26d68d357a
Alvaro Lobato <alobato678@gmail.com>
2017-08-30 15:47:19 +0100

    added index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :lastname)
