d7c787ee48740469be0c90624b6ecbe2b1fa6fc8
laury meredith <meredith.laury@gmail.com>
2017-08-31 16:16:02 +0100

    nearly finished local messaging, needs a cleaning

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,17 @@ class Message < ApplicationRecord
 
   validates :content, presence: true, allow_blank: false
 
+  after_create :broadcast_message
+
   def from?(some_user)
     user == some_user
   end
+
+  def broadcast_message
+    ActionCable.server.broadcast("chatroom_#{chatroom.id}", {
+      message_partial: ApplicationController.renderer.render(partial: "chatrooms/message", locals: { message: self, user_is_messages_author: false }),
+      current_user_id: user.id
+    })
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :sessions_as_student, foreign_key: 'tutor_id'
   has_many :sessions_as_tutor, foreign_key: 'user_id'
+
   has_many :messages, foreign_key: 'sender_id', dependent: :destroy
 
   has_attachment :photo
