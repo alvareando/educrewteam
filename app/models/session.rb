d6158334a84e49d5c9e3b80f67ff68baf5341e36
Alvaro Lobato <alobato678@gmail.com>
2017-09-04 15:15:14 +0100

    Started student and tutor dashboard

--- app/models/session.rb
+++ app/models/session.rb
@@ -7,4 +7,14 @@ class Session < ApplicationRecord
   has_many :session_participations
 
   validates :subject, presence: true
+
+  def ongoing_session?
+    self.time > Time.now && self.date > Date.today
+  end
+
+  def over?
+    !ongoing_session?
+  end
 end
+
+
