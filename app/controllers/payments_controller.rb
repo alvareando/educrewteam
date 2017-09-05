023cb92c7a12acbcd3706df8f2c0bfc4d221f0e3
Alvaro Lobato <alobato678@gmail.com>
2017-09-05 17:28:33 +0100

    stripe and cards

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+  before_action :set_session
+
+  def new
+  end
+
+  def create
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
+
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @session.price_cents,
+      description:  "Payment for session #{@session.id}",
+      currency:     @session.price.currency
+    )
+
+    Payment.create(session_uid: @session.id, payment: charge.to_json, state: 'paid')
+    SessionParticipation.create(session: @session, student: current_user)
+
+    redirect_to session_live_path(@session)
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to session_payment_path(@session)
+  end
+
+  private
+
+  def set_session
+    @session = Session.find(params[:session_id])
+  end
+end
