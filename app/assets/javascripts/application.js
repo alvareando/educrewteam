82c1f7f941bf30c42b96aef7fe6c049c8e346e5c
laury meredith <meredith.laury@gmail.com>
2017-09-07 14:10:55 +0100

    makes the chatting function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,9 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+
+//= require scroll
+
 //= require_tree .
 //= require moment
 //= require bootstrap-datetimepicker
