22a5ba3a6cda8445a3fa129ea47af2ffcc30af0f
laury meredith <meredith.laury@gmail.com>
2017-09-07 15:15:00 +0100

    action cable on production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://www.educrewteam.herokuapp.top/cable"
+  config.action_cable.url = "ws://www.educrewteam.herokuapp.top/cable"
   config.action_cable.allowed_request_origins = [ "http://www.educrewteam.herokuapp.top", "https://www.educrewteam.herokuapp.top" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
