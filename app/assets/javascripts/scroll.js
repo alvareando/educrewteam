d9b31b9b60e50d2faeb5197c6327a11318f871a3
laury meredith <meredith.laury@gmail.com>
2017-09-07 18:38:11 +0100

    done scroll into view last messages

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,10 @@
 function scrollLastMessageIntoView() {
-  const messages = document.querySelectorAll('.message');
-  const lastMessage = messages[messages.length - 1];
-  if (lastMessage) {
-    lastMessage.scrollIntoView();
-  }
+  // const messages = document.querySelectorAll('.message');
+  // const lastMessage = messages[messages.length - 1];
+  // if (lastMessage) {
+  //   lastMessage.scrollIntoView(false);
+  // }
+
+  var messagesContainer = document.querySelector(".messages");
+  messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
