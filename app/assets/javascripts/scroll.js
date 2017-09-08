39314cb486c0b7d8979024ec97e5e9cca51f7bf6
Alvaro Lobato <alobato678@gmail.com>
2017-09-08 10:22:39 +0100

    Done

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,7 @@
-function scrollLastMessageIntoView() {
-  const messages = document.querySelectorAll('.message');
-  const lastMessage = messages[messages.length - 1];
-  if (lastMessage) {
-    lastMessage.scrollIntoView();
-  }
-}
+// function scrollLastMessageIntoView() {
+//   const messages = document.querySelectorAll('.message');
+//   const lastMessage = messages[messages.length - 1];
+//   if (lastMessage) {
+//     lastMessage.scrollIntoView();
+//   }
+// }
