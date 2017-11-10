d48a850fab988c90d60ef7173bbdae6eb186793e
Alvaro Lobato <alobato678@gmail.com>
2017-11-10 01:26:26 +0000

    redoing chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,14 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/serviceworker.js')
+  .then(function(reg) {
+     console.log('Service worker change, registered the service worker');
+  });
+}
+// Otherwise, no push notifications :(
+else {
+  console.error('Service worker is not supported in this browser');
+}
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
